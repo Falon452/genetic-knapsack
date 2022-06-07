b066fc87ffc08a2ee8c130b1daea292b884fb265
Falon <falon4522@gmail.com>
2022-06-07 21:06:52 +0200

    dynamic knapsack implementation

--- Dynamic.py
+++ Dynamic.py
@@ -1,6 +1,19 @@
-
-
-
 def dynamic_solve(n, value, weight, capacity):
-    result = 0
-    return result
+    dp = [0 for i in range(capacity + 1)]  # Making the dp array
+
+    for i in range(1, n + 1):  # taking first i elements
+        for w in range(capacity, 0, -1):  # starting from back,so that we also have data of
+            # previous computation when taking i-1 items
+            if weight[i - 1] <= w:
+                # finding the maximum value
+                dp[w] = max(dp[w], dp[w - weight[i - 1]] + value[i - 1])
+
+    return dp[capacity]  # returning the maximum value of knapsack
+
+
+if __name__ == '__main__':
+    val = [60, 100, 120]
+    wt = [10, 20, 30]
+    W = 50
+    n = len(val)
+    assert dynamic_solve(n, val, wt, W) == 220
