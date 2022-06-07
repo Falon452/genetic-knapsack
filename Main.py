51af3bd2b905d08c5f5fc4f0d79d69f7e7be1e17
Falon <falon4522@gmail.com>
2022-06-07 22:40:55 +0200

    deleted old test cases too hard

--- Main.py
+++ Main.py
@@ -1,24 +1,106 @@
-from Reader import parse_file, get_optimum
+from Reader import parse_file, get_optimum, get_filenames_in_dir
 from Dynamic import dynamic_solve
 from Genetic import genetic_solve
 # from OR_Tools import OR_Tools_solve
 from TimeLimit import time_limit, TimeoutException
-from Stats import relative_error
+from Stats import accuracy
+import pandas as pd
 import time
 
-MAX_TIME_IN_SEC = 2
+from timeit import default_timer as timer
+from datetime import timedelta
 
-if __name__ == '__main__':
-    dynamic_res, or_tools_res, genetic_res = None, None, None
-    n, value, weight, capacity = parse_file("problem_instances/n_400_c_1000000_g_2_f_0.1_eps_0_s_100/test.in")
+MAX_TIME_IN_SEC = 5
 
+INPUT_PATH = "instances_01_KP/low-dimensional/"
+OPTIMUM_PATH = "instances_01_KP/low-dimensional-optimum/"
+
+
+def compare_algorithms(filenames, save_path):
+    data = {'Filename': [],
+            'Length': [],
+            'DP res': [],
+            'OR res': [],
+            'Gen res': [],
+            'Optimum': [],
+            'DP acc': [],
+            'OR acc': [],
+            'Gen acc': [],
+            'DP time': [],
+            'OR time': [],
+            'Gen time': []}
+
+    for filename in filenames:
+        filepath = INPUT_PATH + filename
+        optimum_filepath = OPTIMUM_PATH + filename
+        data['Filename'].append(filename)
+        n, value, weight, capacity = parse_file(filepath)
+        data['Length'].append(n)
+
+        optimum = get_optimum(optimum_filepath)
+        data['Optimum'].append(optimum)
+
+        # genetic
+        start = timer()
+        gen_res = test_function(n, value, weight, capacity, genetic_solve)
+        end = timer()
+        data['Gen res'].append(gen_res)
+        data['Gen acc'].append(accuracy(gen_res, optimum))
+        data['Gen time'].append(timedelta(seconds=end - start).microseconds)
+
+        # dynamic
+        start = timer()
+        dp_res = test_function(n, value, weight, capacity, dynamic_solve)
+        end = timer()
+        data['DP res'].append(dp_res)
+        data['DP acc'].append(accuracy(dp_res, optimum))
+        data['DP time'].append(timedelta(seconds=end - start).microseconds)
+
+        # or tools
+        start = timer()
+        # or_res = test_function(n, value, weight, OR_Tools_solve)
+        or_res = None
+        end = timer()
+        data['OR res'].append(or_res)
+        data['OR acc'].append(accuracy(or_res, optimum))
+        data['OR time'].append(timedelta(seconds=end - start).microseconds)
+
+
+    df = pd.DataFrame(data)
+
+    df.to_csv(f'{save_path}.csv', index=False)
+    return df
+
+
+def test_function(n, value, weight, capacity, function):
+    res = None
     try:
         with time_limit(MAX_TIME_IN_SEC):
-            dynamic_res = dynamic_solve(n, value, weight, capacity)
+            res = function(n, value, weight, capacity)
     except TimeoutException as e:
-        print(f"Dynamic timeout after {MAX_TIME_IN_SEC} seconds")
+        print(f"{function.__name__} timeout after {MAX_TIME_IN_SEC} seconds")
 
-    print(dynamic_res)
-    optimum = get_optimum("n_400_c_1000000_g_2_f_0.1_eps_0_s_100")
-    print(optimum)
-    print(relative_error(dynamic_res, optimum))
+    return res
+
+
+if __name__ == '__main__':
+    filenames = get_filenames_in_dir(INPUT_PATH)
+    df = compare_algorithms(filenames, "result")
+    print(df.head())
+
+    # dynamic_res, or_tools_res, genetic_res = None, None, None
+    # n, value, weight, capacity = parse_file("instances_01_KP/low-dimensional/f4_l-d_kp_4_11")
+    #
+    # try:
+    #     with time_limit(MAX_TIME_IN_SEC):
+    #         dynamic_res = dynamic_solve(n, value, weight, capacity)
+    # except TimeoutException as e:
+    #     print(f"Dynamic timeout after {MAX_TIME_IN_SEC} seconds")
+    #
+    # print(dynamic_res)
+    # optimum = get_optimum("instances_01_KP/low-dimensional-optimum/f4_l-d_kp_4_11")
+    # print(optimum)
+    # print(accuracy(dynamic_res, optimum))
+
+
+
--- Reader.py
+++ Reader.py
@@ -1,44 +1,37 @@
+import os
 
 
 def parse_file(path):
     """Parses a file in a following format:
 
-    3
-    1 3 8
-    2 2 8
-    3 9 1
-    10
-
-    3 - number of items
-    1 3 8 - 1 is ID, 3 is value, 8 is weight
+    n, capacity
+    value1, weight1
+    value2, weight2
     ...
-    10 - backpack_capacity
+    valuen, weightn
     """
     value = []
     weight = []
     with open(path, "r") as f:
-        n = int(f.readline().strip())
+        n, capacity = tuple(map(int, f.readline().split()))
         for i in range(n):
-            id, v, w = tuple(map(int, f.readline().split()))
+            v, w = tuple(map(int, f.readline().split()))
             value.append(v)
             weight.append(w)
-
-        capacity = int(f.readline())
 
     return n, value, weight, capacity
 
 
-def get_optimum(filename, path_to_optima="optima.csv"):
+def get_optimum(path_to_optima):
     with open(path_to_optima, "r") as f:
-        lines = f.readlines()
-        for line in lines[1:]:
-            name, optimum = line.split(",")
-            if name == filename:
-                return optimum
+        optimum = int(f.readline())
+        return optimum
 
-    raise FileNotFoundError("Could not find filename.")
+
+def get_filenames_in_dir(dirpath):
+    return os.listdir(dirpath)
 
 
 if __name__ == '__main__':
-    n, value, weight, capacity = parse_file("problem_instances/n_400_c_1000000_g_2_f_0.1_eps_0_s_100/test.in")
+    n, value, weight, capacity = parse_file("instances_01_KP/low-dimensional/f1_l-d_kp_10_269")
     print(n, value, weight, capacity)
